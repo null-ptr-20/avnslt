b936a0f73faeb7c9ad25b739b8f1c20d26de1945
ryantz <ryantanzhi1997@gmail.com>
2025-03-20 17:17:40 +0800

    update main.rs :3

--- rust_code/avnslt/src/main.rs
+++ rust_code/avnslt/src/main.rs
@@ -1,3 +1,17 @@
+
 fn main() {
-    println!("Hello, world!");
+    let select = Greeting::Hello;
+
+    let choice = match select {
+        Greeting::Hello => println!("HELLO"),
+        Greeting::Bye => println!("BYE"),
+        _ => println!("invalid"),
+    };
+
+    choice
+}
+
+ enum Greeting {
+    Hello,
+    Bye
 }
