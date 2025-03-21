688942cce46d1f1ca16bda95dbe5952902be9b1f
ryantz <ryantanzhi1997@gmail.com>
2025-03-21 13:51:31 +0800

    change file struct

---
+++ rust_code/target/debug/avnslt.d
@@ -0,0 +1,1 @@
+/home/ryantz/repos/avnslt/rust_code/target/debug/avnslt: /home/ryantz/repos/avnslt/rust_code/src/main.rs
---
+++ rust_code/target/debug/deps/avnslt-ecb24902415335df.d
@@ -0,0 +1,5 @@
+/home/ryantz/repos/avnslt/rust_code/avnslt/target/debug/deps/avnslt-ecb24902415335df: src/main.rs
+
+/home/ryantz/repos/avnslt/rust_code/avnslt/target/debug/deps/avnslt-ecb24902415335df.d: src/main.rs
+
+src/main.rs:
