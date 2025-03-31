1642adecb23af19ddb5f38ff46e4af9b68f46a55
ryantz <ryantanzhi1997@gmail.com>
2025-04-01 01:36:34 +0800

    update gitignore

---
+++ rs_avnslt/target/debug/build/winapi-6f7d8576ac7857f0/build_script_build-6f7d8576ac7857f0.d
@@ -0,0 +1,5 @@
+C:\Users\ryant\repos\avnslt\rs_avnslt\target\debug\build\winapi-6f7d8576ac7857f0\build_script_build-6f7d8576ac7857f0.exe: C:\Users\ryant\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\winapi-0.3.9\build.rs
+
+C:\Users\ryant\repos\avnslt\rs_avnslt\target\debug\build\winapi-6f7d8576ac7857f0\build_script_build-6f7d8576ac7857f0.d: C:\Users\ryant\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\winapi-0.3.9\build.rs
+
+C:\Users\ryant\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\winapi-0.3.9\build.rs:
---
+++ rs_avnslt/target/debug/deps/avnslt-8ed58fd88fdb8de4.d
@@ -0,0 +1,10 @@
+C:\Users\ryant\repos\avnslt\rs_avnslt\target\debug\deps\libavnslt-8ed58fd88fdb8de4.rmeta: src\lib.rs src\colors.rs src\ui.rs src\file.rs
+
+C:\Users\ryant\repos\avnslt\rs_avnslt\target\debug\deps\libavnslt-8ed58fd88fdb8de4.rlib: src\lib.rs src\colors.rs src\ui.rs src\file.rs
+
+C:\Users\ryant\repos\avnslt\rs_avnslt\target\debug\deps\avnslt-8ed58fd88fdb8de4.d: src\lib.rs src\colors.rs src\ui.rs src\file.rs
+
+src\lib.rs:
+src\colors.rs:
+src\ui.rs:
+src\file.rs:
